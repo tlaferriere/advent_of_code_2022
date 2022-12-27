b360336222204bfbe58292b8a76199303a7f90dc
Thomas Laferriere <thomas.laferriere@polymtl.ca>
2022-12-27 11:01:45 -0500

    day 6 problem 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,19 +3,21 @@ use std::fs::File;
 
 use std::io::{BufRead, BufReader, Read};
 
+const MARKER_LEN: usize = 14;
+
 fn main() -> std::io::Result<()> {
     let mut f = File::open("input.txt")?;
     let mut buf = vec![];
     f.read_to_end(&mut buf)?;
-    for (step, window) in buf.as_slice().windows(4).enumerate() {
-        let mut char_set = HashSet::with_capacity(4);
+    for (step, window) in buf.as_slice().windows(MARKER_LEN).enumerate() {
+        let mut char_set = HashSet::with_capacity(MARKER_LEN);
         for c in window {
             if !char_set.insert(*c) {
                 break;
             }
         }
-        if char_set.len() == 4 {
-            let pos = step + 4;
+        if char_set.len() == MARKER_LEN {
+            let pos = step + MARKER_LEN;
             println!("{pos}");
             break;
         }
