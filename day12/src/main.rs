5228787567105ae112e291c4410eb300f0c1db70
tlaferriere <t.laferriere@protonmail.com>
2023-05-18 11:14:09 -0400

    Day 12 part 2 complete

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -7,31 +7,49 @@ use std::io::{BufRead, BufReader, Read};
 use std::rc::Rc;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-struct Pos((i32, i32), Rc<Vec<Vec<usize>>>);
+struct Pos(i32, i32);
 
 impl Pos {
+    pub(crate) fn success(&self, topo: &Vec<Vec<usize>>) -> bool {
+        *topo
+            .get(self.1 as usize)
+            .expect("Moved out of y bounds")
+            .get(self.0 as usize)
+            .expect("Moved out of x bounds")
+            == 0
+    }
+    pub(crate) fn heuristic(&self, topo: &Vec<Vec<usize>>, lows: &Vec<Pos>) -> u32 {
+        let mut closest_dist = topo.len() as u32 * 2;
+        for low in lows {
+            let dist = self.distance(low);
+            if dist < closest_dist {
+                closest_dist = dist;
+            }
+        }
+        closest_dist
+    }
     fn distance(&self, other: &Pos) -> u32 {
-        (abs(self.0 .0 - other.0 .0) + abs(self.0 .1 - other.0 .1)) as u32
+        (abs(self.0 - other.0) + abs(self.1 - other.1)) as u32
     }
 
-    fn successors(&self) -> Vec<(Pos, u32)> {
-        let Pos((x, y), topo) = self;
+    fn successors(&self, topo: &Vec<Vec<usize>>) -> Vec<(Pos, u32)> {
+        let Pos(x, y) = self;
         let max_height = topo
             .get(*y as usize)
             .expect("Moved out of y bounds")
             .get(*x as usize)
             .expect("Moved out of x bounds");
         vec![
-            Pos((x + 1, *y), topo.clone()),
-            Pos((x - 1, *y), topo.clone()),
-            Pos((*x, y + 1), topo.clone()),
-            Pos((*x, y - 1), topo.clone()),
+            Pos(x + 1, *y),
+            Pos(x - 1, *y),
+            Pos(*x, y + 1),
+            Pos(*x, y - 1),
         ]
         .into_iter()
-        .filter(|Pos((x, y), topo)| {
+        .filter(|Pos(x, y)| {
             let Some(row) = topo.get(*y as usize) else { return false; };
             let Some(z) = row.get(*x as usize) else { return false; };
-            *z <= *max_height + 1
+            *max_height - 1 <= *z
         })
         .map(|p| (p, 1))
         .collect()
@@ -49,6 +67,7 @@ fn main() -> std::io::Result<()> {
     let mut start: Option<(i32, i32)> = None;
     let mut end: Option<(i32, i32)> = None;
     let mut topo = vec![];
+    let mut lows = vec![];
     for (j, line) in reader.lines().enumerate() {
         let line = line?;
         let mut row = Vec::with_capacity(line.len());
@@ -67,21 +86,26 @@ fn main() -> std::io::Result<()> {
                         unreachable!()
                     }
                 },
+                Some(0) => {
+                    lows.push(Pos(i as i32, j as i32));
+                    0
+                }
                 Some(h) => *h,
             });
         }
         topo.push(row);
     }
 
-    let topo = Rc::new(topo);
-    let start = Pos(start.expect("Start position not found"), topo.clone());
-    let end = Pos(end.expect("Start position not found"), topo.clone());
+    let (xs, ys) = start.expect("Start position not found");
+    let start = Pos(xs, ys);
+    let (xe, ye) = end.expect("Start position not found");
+    let end = Pos(xe, ye);
 
     let result = astar(
-        &start,
-        |p| p.successors(),
-        |p| p.distance(&end),
-        |p| p.0 == end.0,
+        &end,
+        |p| p.successors(&topo),
+        |p| p.heuristic(&topo, &lows),
+        |p| p.success(&topo),
     );
     // .expect("Path could not be found");
     println!("{}", result.unwrap().1);
